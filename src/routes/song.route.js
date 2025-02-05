79ea97cc489b1c80e2730fefa959eed8444ccfb8
mari-rj <maryam.rajabi@dci-student.org>
2025-02-05 14:05:58 +0100

    Song-Rautes-Controllers

--- src/routes/song.route.js
+++ src/routes/song.route.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { getAllSongs } from "../controller/song.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get('/',(req, res)=> {
-    res.send('song route with GET method');
-});
+router.get("/", protectRoute, requireAdmin, getAllSongs);
+router.get("/featured", getFeaturedSongs);
+router.get("/made-for-you", getMadeForYouSongs);
+router.get("/trending", getTrendingSongs);
 
 export default router;
